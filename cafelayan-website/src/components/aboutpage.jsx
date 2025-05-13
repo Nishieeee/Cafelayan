a084b5bcf2876447cdaa5d455cdca71a9e7f0332
Cleeein <pagaroganjhonclein@gmail.com>
2025-05-13 17:49:11 +0800

    add more content to about page and minor adjustments

--- cafelayan-website/src/components/aboutpage.jsx
+++ cafelayan-website/src/components/aboutpage.jsx
@@ -8,7 +8,9 @@ function Aboutpage() {
       <section className="py-10 px-20 grid grid-cols-7 h-screen">
         <div className="col-span-4 ">
           <div className="border-b border-green-900 w-130">
-            <h2 className="text-green-950 text-[2.5rem]">Cafelayan Hydroponic Farm</h2>
+            <h2 className="text-green-950 text-[2.5rem]">
+              Cafelayan Hydroponic Farm
+            </h2>
           </div>
           <div className="text-[1rem] p-3">
             <h3 className="text-[2rem] text-green-950">Our Mission</h3>
@@ -24,18 +26,37 @@ function Aboutpage() {
               innovative hydroponic techniques.
             </p>
           </div>
-          <div></div>
-        </div>
-        <div className="col-span-3 gap-10">
-            <div className="grid grid-cols-2">
-              <div className="col-span-1 p-2">
-                <img src={Contentimage} alt="Cafelayan Image Sign" className="rounded-xl"/>
-              </div>
-              <div className="col-span-1 flex flex-col p-1">
-                <img src={Contentimage2} alt="Cafelayan Owner Jeffrey Sereno" className="rounded-xl p-2"/>
-                <img src={Contentimage3} alt="Lettuce" className="rounded-xl p-2"/>
-              </div>
+          <div className="grid grid-cols-2 text-[1.5rem] text-green-950">
+            <div>
+              <h5>Organic making</h5>
             </div>
+            <div>
+              <h5>Certified Products</h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-3">
+          <div className="grid grid-cols-2">
+            <div className="col-span-1 p-2">
+              <img
+                src={Contentimage}
+                alt="Cafelayan Image Sign"
+                className="rounded-xl"
+              />
+            </div>
+            <div className="col-span-1 flex flex-col p-1">
+              <img
+                src={Contentimage2}
+                alt="Cafelayan Owner Jeffrey Sereno"
+                className="rounded-xl p-2"
+              />
+              <img
+                src={Contentimage3}
+                alt="Lettuce"
+                className="rounded-xl p-2"
+              />
+            </div>
+          </div>
         </div>
       </section>
       <section className="bg-lime-500">
